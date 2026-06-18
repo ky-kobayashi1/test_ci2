a1628926e7354af1e5fdc25f539bc2ae7de45a9f
ky-kobayashi1 <ky-kobayashi@psc-inc.co.jp>
2026-06-18 14:57:43 +0900

    no message

--- calc.py
+++ calc.py
@@ -1,1 +1,9 @@
-print("こんにちは")
+"""Minimal sample functions used by the pytest CI example."""
+
+
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a + b
--- test_calc.py
+++ test_calc.py
@@ -8,4 +8,4 @@ def test_add():
 
 
 def test_sub():
-    assert sub(5, 2) == 3
+    assert sub(5, 2) == 7
